9e19905ca95cdd8cb2614c25d5213a907aa63e15
Dhairya Bhatt <dhairyabhatt946@gmail.com>
2024-11-03 19:45:11 +0530

    Register

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const colors = require('colors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //dotenv config
 dotenv.config();
+
+//mongodb connection
+connectDB();
 
 //rest object
 const app = express();
@@ -14,11 +18,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 //routes
-app.get('/', (req, res) => {
-    req.statusCode(200).send({
-        message: "server running",
-    });
-});
+app.use('/api/v1/user', require('./routes/userRoutes'));
 
 const port = process.env.PORT || 8080
 //listen port
