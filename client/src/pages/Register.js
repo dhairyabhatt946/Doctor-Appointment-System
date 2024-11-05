3be0310f3bd156c988683ffe2d292070cbb37303
Dhairya Bhatt <dhairyabhatt946@gmail.com>
2024-11-05 19:09:57 +0530

    react-redux

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import {Form, Input, message} from 'antd';
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 import '../styles/RegisterStyles.css';
 import {Link, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 const Register = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   //form handler
   const onfinishHandler = async (values) => {
     try {
+      dispatch(showLoading());
       const res = await axios.post('/api/v1/user/register', values);
+      dispatch(hideLoading());
       if(res.data.success) {
         message.success('Registered Successfully!');
         navigate('/login');
@@ -19,6 +24,7 @@ const Register = () => {
         message.error(res.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       console.log(error);
       message.error('Something Went Wrong');
     }
